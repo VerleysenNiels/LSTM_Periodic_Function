e8dc5a47b085b69b2e798808110616c50099c594
Niels Verleysen <nlsverleysen99@gmail.com>
2019-12-21 21:29:10 +0100

    Add periodic function class and add ability to apply gaussian noise.

---
+++ PeriodicFunction.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 21 19:54:30 2019
+
+@author: Niels Verleysen
+
+Class that represents a periodic function (sinusoid) with the possibility to add different types of noise.
+First initialize the function and specify the amplitude and frequency of the sinusoid.
+Then you can add noise to the function by calling the corresponding functions.
+Get a value for a given timepoint by calling value.
+
+An example is given below.
+"""
+
+import math
+import numpy as np
+
+class PeriodicFunction:
+    
+    def __init__(self, amplitude, frequency):
+        self.gaussian = False
+        self.additionalF = False
+        self.asymmetric = False
+        self.amplitude = amplitude
+        self.frequency = frequency
+        
+    def add_gaussian_noise(self, deviation):
+        self.gaussian = True
+        self.gaussian_dev = deviation
+        
+    def add_additional_frequency(self, amplitude, frequency):
+        print("ToDo")
+        
+    def add_asymmetric_distributed_noise(self):
+        print("ToDo")
+        
+    def value(self, time):
+        val = math.sin(time)
+        
+        if self.gaussian:
+            noise = np.random.normal(0, self.gaussian_dev, 1)
+            val = val + noise[0]
+        
+        return val
+    
+if __name__ == '__main__':
+    function = PeriodicFunction(15, 0.3)
+    v = function.value(16)
+    print(v)
+    function.add_gaussian_noise(1.3)
+    v = function.value(33)
+    print(v)       
