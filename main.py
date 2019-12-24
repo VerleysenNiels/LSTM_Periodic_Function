729ded5e051f166536f39b3e5e4ea11a4cb9dbbb
Niels Verleysen <nlsverleysen99@gmail.com>
2019-12-24 16:09:04 +0100

    Implemented build_dataset and finished run.

--- main.py
+++ main.py
@@ -14,31 +14,55 @@ This should then be done for varying frequencies, varying k and varying noise of
 """
 
 from LSTM import PeriodicFunctionLSTM
+from random import randint
+from random import seed
 
 """Define global variables"""
 EPOCHS = 15
 BATCH_SIZE = 20
+DATASET_SIZE = 100
+TESTSET_SIZE = 50
+SEED = 36947
 
 """
-Build a training dataset with values from given function with given sampling rate.
+Build a training or testing dataset with values from given function with given sampling rate.
 Group k previous true values as next input for the model.
 """
-def build_dataset(function, k, sampling_rate):
-    print("ToDo")
+def build_dataset(function, k, sampling_rate, test=False):
+    values = []
+    X = []
+    Y = []
+    size = TESTSET_SIZE if test else DATASET_SIZE
+    startpoint = randint(0,100)
+    
+    #Generate values
+    for i in range(startpoint, size + k + startpoint):
+        values.append(function.value(sampling_rate * i))
+    
+    #Use generated values to build the training data
+    for i in range(0, DATASET_SIZE):
+        index = i + k # current value for Y; we start from element k in values list
+        Y.append(values[index])
+        x = []  #List with k previous values
+        for j in range (index - k, index):
+            x.append(values[j-1])
+        X.append(x)
+    
+    return X,Y
 
 """
 Run basic experiment:
     Build the model with given specifications.
-    Use build_dataset function to build the dataset.
+    Use build_dataset function to build the training data.
     Train the model on this dataset.
     Test the accuracy of the model.
 """
 def run(function, architecture, k, sampling_rate):
     model = PeriodicFunctionLSTM(architecture,[k])
-    dataset = build_dataset(function, k, sampling_rate)
-    model.train(dataset[0], dataset[1], EPOCHS, BATCH_SIZE)
-    
-    #ToDo: test model and return accuracy
+    X,Y = build_dataset(function, k, sampling_rate)
+    model.train(X, Y, EPOCHS, BATCH_SIZE)
+    X,Y = build_dataset(function, k, sampling_rate, test=True)
+    return model.evaluate(x=X, y=Y)
 
 """
 MAIN
@@ -46,4 +70,5 @@ Use run function to test model on function with different types of noise, differ
 Collect the accuracies from these tests.
 """
 if __name__ == '__main__':
+    seed(SEED)
     print("ToDo")
