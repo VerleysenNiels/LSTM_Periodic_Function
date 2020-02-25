0814ff6b35e5e8123da6936cb3b688d21a3c72fc
Niels Verleysen <nlsverleysen99@gmail.com>
2020-02-25 14:13:39 +0100

    Run experiment with a combination of gaussian noise and disturbing frequency

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     """Determine function"""
     training_function = PeriodicFunction(10, 0.016667)
     training_function.add_gaussian_noise(2)
-    training_function.add_additional_frequency(1, 0.001)
+    training_function.add_additional_frequency(1, 2)
 
     test_function = PeriodicFunction(10, 0.016667)
     
