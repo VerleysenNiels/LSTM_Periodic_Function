9d1826bf61ab1fcdd73501f50692530aa78e7537
Niels Verleysen <nlsverleysen99@gmail.com>
2020-03-22 15:29:45 +0100

    Outliers (P=1%)

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     
     """Determine function"""
     training_function = PeriodicFunction(10, 0.016667)
-    training_function.add_random_outliers(0.0001, 4, 10)
+    training_function.add_random_outliers(0.01, 4, 10)
     #training_function.add_disturbing_frequency(4, 2)
 
     test_function = PeriodicFunction(10, 0.016667)
