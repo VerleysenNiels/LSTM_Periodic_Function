817e2b77a06258d940d169e3e4244bb50639f670
Niels Verleysen <nlsverleysen99@gmail.com>
2020-03-22 13:28:11 +0100

    Run on noiseless signal with changed network

--- PeriodicFunctionLSTM.py
+++ PeriodicFunctionLSTM.py
@@ -11,7 +11,7 @@ Trained weights can then be used to predict values of the periodic function.
 """
 
 from keras.models import Model
-from keras.layers import LSTM, Input
+from keras.layers import LSTM, Input, Dense
 from keras.callbacks import ModelCheckpoint
 from keras import losses
 import numpy as np
@@ -22,10 +22,11 @@ class PeriodicFunctionLSTM:
     def __init__(self, architecture, k):
         inputs = Input(shape=(k,1))
         l = inputs
-        for layer in architecture:
-            l = LSTM(int(layer), return_sequences=True)(l)
-            
-        outputs = LSTM(1)(l)
+        for i in range(0, len(architecture)-1):
+            l = LSTM(int(architecture[i]), return_sequences=True)(l)
+
+        l = LSTM(int(architecture[-1]), return_sequences=False)(l)
+        outputs = Dense(1)(l)
         
         self.model = Model(inputs=inputs, outputs=outputs)
         self.model.compile(loss=losses.mean_absolute_error, optimizer='Adam', metrics=['accuracy', 'mae'])
