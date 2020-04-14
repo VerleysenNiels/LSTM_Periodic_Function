e92893183d1eb20dfe069e91d270cced84c52237
Niels Verleysen <nlsverleysen99@gmail.com>
2020-04-14 22:34:21 +0200

    Architecture search: repeat previous example with gaussian noise (dev 1.5)

--- main.py
+++ main.py
@@ -90,6 +90,7 @@ if __name__ == '__main__':
     """Determine function"""
     training_function = PeriodicFunction(10, 0.016667)
     training_function.add_disturbing_signal(4, 0.4)
+    training_function.add_gaussian_noise(1.5)
 
     test_function = PeriodicFunction(10, 0.016667)
     test_function.add_disturbing_signal(4, 0.4)
